3fd46bf7675eb8d5d32fb1dc36e739c93ccf2778
lucasviniciusbatistacosta <lucasviniciusbatistacosta@gmail.com>
2024-07-10 17:21:49 -0300

    add client methods

--- SFManagement/Controllers/ClientController.cs
+++ SFManagement/Controllers/ClientController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SFManagement.Models;
 
 namespace SFManagement.Controllers
 {
@@ -13,10 +14,23 @@ namespace SFManagement.Controllers
             _logger = logger;
         }
 
+        [HttpGet]
+        public List<Client> Get() => new List<Client>();
 
-        public IActionResult Index()
-        {
-            return View();
-        }
+        [HttpGet]
+        [Route("{id}")]
+        public Client Get(Guid id) => new Client();
+
+        [HttpDelete]
+        [Route("{id}")]
+        public Client Delete(Guid id) => new Client();
+
+        [HttpPost]
+        [Route("")]
+        public Client Post(Client model) => model;
+
+        [HttpPut]
+        [Route("{id}")]
+        public Client Put(Guid id, Client model) => model;
     }
 }
