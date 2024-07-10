3a4bf61202530057acf8a257bfb2437f60262be9
lucasviniciusbatistacosta <lucasviniciusbatistacosta@gmail.com>
2024-07-10 17:00:55 -0300

    add client validator.

--- SFManagement/Program.cs
+++ SFManagement/Program.cs
@@ -1,3 +1,8 @@
+using FluentValidation;
+using SFManagement.Models;
+using SFManagement.Models.Validators;
+using System;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,13 +12,15 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddScoped<IValidator<Client>, ClientValidator>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-	app.UseSwagger();
-	app.UseSwaggerUI();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
