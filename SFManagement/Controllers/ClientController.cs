eee8d1d2aea599ccf589a883babd41ab4f990f52
lucasviniciusbatistacosta <lucasviniciusbatistacosta@gmail.com>
2024-07-10 17:01:04 -0300

    add client controller.

---
+++ SFManagement/Controllers/ClientController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SFManagement.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ClientController : ControllerBase
+    {
+        private readonly ILogger<ClientController> _logger;
+
+        public ClientController(ILogger<ClientController> logger)
+        {
+            _logger = logger;
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
