bd47b1281d3ad1a18e7f25925ec12b32a9fd7a90
lucasviniciusbatistacosta <lucasviniciusbatistacosta@gmail.com>
2024-08-05 17:53:07 -0300

    add client endpoint.

--- Controllers/ClientController.cs
+++ Controllers/ClientController.cs
@@ -13,10 +13,16 @@ namespace SFManagement.Controllers
     public class ClientController : BaseApiController<Client, ClientRequest, ClientResponse>
     {
         private readonly ILogger<ClientController> _logger;
+        private readonly ClientService _clientService;
 
-        public ClientController(ClientService service, ILogger<ClientController> logger, IMapper mapper) : base(service, mapper)
+        public ClientController(ClientService service, ILogger<ClientController> logger, IMapper mapper, ClientService clientService) : base(service, mapper)
         {
             _logger = logger;
+            _clientService = clientService;
         }
+
+        [HttpGet]
+        [Route("balance/{id}")]
+        public async Task<BalanceResponse> Balance(Guid clientId) => await _clientService.GetBalance(clientId);
     }
 }
--- Services/ClientService.cs
+++ Services/ClientService.cs
@@ -1,5 +1,7 @@
 ﻿using SFManagement.Data;
 using SFManagement.Models;
+using SFManagement.ViewModels;
+using System.Data.Entity;
 
 namespace SFManagement.Services
 {
@@ -7,6 +9,12 @@ namespace SFManagement.Services
     {
         public ClientService(DataContext context) : base(context)
         {
+        }
+
+        public async Task<BalanceResponse> GetBalance(Guid clientId)
+        {
+            var client = (await context.Clients.Include(x => x.BankTransactions).FirstOrDefaultAsync(x => x.Id == clientId));
+            return new BalanceResponse(client.BankTransactions);
         }
     }
 }
