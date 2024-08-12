4a34b40f2efeee31c2b54722660b05a7abce464c
lucasviniciusbatistacosta <lucasviniciusbatistacosta@gmail.com>
2024-08-12 17:54:44 -0300

    add nickname and wallet.

---
+++ Controllers/NicknameController.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SFManagement.Models;
+using SFManagement.Services;
+using SFManagement.ViewModels;
+
+namespace SFManagement.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("[controller]")]
+    public class NicknameController : BaseApiController<Nickname, NicknameRequest, NicknameResponse>
+    {
+        public NicknameController(BaseService<Nickname> service, IMapper mapper) : base(service, mapper)
+        {
+        }
+    }
+}
---
+++ Controllers/WalletController.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SFManagement.Models;
+using SFManagement.Services;
+using SFManagement.ViewModels;
+
+namespace SFManagement.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("[controller]")]
+    public class WalletController : BaseApiController<Wallet, WalletRequest, WalletResponse>
+    {
+        public WalletController(BaseService<Wallet> service, IMapper mapper) : base(service, mapper)
+        {
+        }
+    }
+}
