fb926de9ec543aab4c91489ad9dd469d83c2ca7d
Lucas Costa <microsoft-e6656b3e.vehicular311@simplelogin.com>
2024-12-03 18:11:26 -0300

    update client balance.

--- ViewModels/BalanceResponse.cs
+++ ViewModels/BalanceResponse.cs
@@ -6,7 +6,11 @@ namespace SFManagement.ViewModels
     {
         public BalanceResponse(IEnumerable<BankTransaction> transactions)
         {
-            Value = transactions.Where(x=> !x.DeletedAt.HasValue).Sum(x => x.BankTransactionType == Enums.BankTransactionType.Income ? x.Value : decimal.Negate(x.Value));
+            Value = transactions.Where(x => !x.DeletedAt.HasValue
+                                            && ((!x.ApprovedAt.HasValue) || (x.ApprovedAt.HasValue && x.LinkedToId.HasValue)))
+                                .Sum(x => x.BankTransactionType == Enums.BankTransactionType.Income ? x.Value : decimal.Negate(x.Value));
+
+            //TODO: ACRESCENTAR WALLETTRANSACTIONS.
         }
 
         public decimal? Value { get; set; }
