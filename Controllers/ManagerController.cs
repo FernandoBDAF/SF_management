d331a34671d6a6d4f5d6625bf6a762b3b6e95ce9
Lucas Costa <microsoft-e6656b3e.vehicular311@simplelogin.com>
2024-12-05 17:48:03 -0300

    add manager transaction list.

--- Controllers/ManagerController.cs
+++ Controllers/ManagerController.cs
@@ -18,19 +18,25 @@ namespace SFManagement.Controllers
 
         private readonly IMapper _mapper;
 
-        public ManagerController(BaseService<Manager> service, IMapper mapper, WalletService walletService, ManagerService managerService) : base(service, mapper)
+        private readonly TransactionService _transactionService;
+
+        public ManagerController(BaseService<Manager> service, IMapper mapper, WalletService walletService, ManagerService managerService, TransactionService transactionService) : base(service, mapper)
         {
             _mapper = mapper;
             _walletService = walletService;
             _managerService = managerService;
+            _transactionService = transactionService;
         }
 
         [HttpGet("/wallets/{managerId}")]
         public async Task<List<WalletResponse>> GetWalletsByManagerId(Guid managerId) => _mapper.Map<List<WalletResponse>>(await _walletService.GetWalletsByManagerId(managerId));
 
-
         [HttpGet]
         [Route("balance/{managerId}")]
         public async Task<BalanceResponse> Balance(Guid managerId) => await _managerService.GetBalance(managerId);
+
+        [HttpGet]
+        [Route("transactions/{managerId}/{startDate?}/{endDate?}/{quantity?}/{page?}")]
+        public async Task<TableResponse<TransactionResponse>> Transactions(Guid managerId, DateTime? startDate = null, DateTime? endDate = null, int quantity = 100, int page = 0) => await _transactionService.GetManagerTransactions(managerId, startDate, endDate, quantity, page);
     }
 }
--- Services/TransactionService.cs
+++ Services/TransactionService.cs
@@ -1,6 +1,6 @@
-﻿using SFManagement.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using SFManagement.Data;
 using SFManagement.ViewModels;
-using System.Data.Entity;
 
 namespace SFManagement.Services
 {
@@ -11,6 +11,36 @@ namespace SFManagement.Services
         public TransactionService(DataContext context)
         {
             _context = context;
+        }
+
+        public async Task<TableResponse<TransactionResponse>> GetManagerTransactions(Guid managerId, DateTime? startDate, DateTime? endDate, int quantity, int page)
+        {
+            var response = new TableResponse<TransactionResponse>()
+            {
+                Page = page,
+                Show = quantity
+            };
+
+            var walletTransactionsQuery = _context.WalletTransactions.Include(x => x.Wallet).Where(x => x.Wallet != null && x.Wallet.ManagerId == managerId);
+
+            if (startDate.HasValue)
+            {
+                walletTransactionsQuery = walletTransactionsQuery.Where(x => x.Date >= startDate.Value);
+            }
+
+            if (endDate.HasValue)
+            {
+                walletTransactionsQuery = walletTransactionsQuery.Where(x => x.Date <= endDate.Value);
+            }
+
+            response.Total = walletTransactionsQuery.Count();
+
+            var allTransactions = new List<TransactionResponse>();
+            allTransactions.AddRange((walletTransactionsQuery.ToList()).Select(x => new TransactionResponse(x)));
+
+            response.Data = allTransactions.OrderBy(x => x.Date).Skip(page * quantity).Take(quantity).ToList();
+
+            return response;
         }
 
         public async Task<TableResponse<TransactionResponse>> GetTransactions(Guid clientId, DateTime? startDate, DateTime? endDate, int quantity, int page)
@@ -37,7 +67,7 @@ namespace SFManagement.Services
             }
 
             response.Total = bankTransactionsQuery.Count() + walletTransactionsQuery.Count();
-            
+
             var allTransactions = new List<TransactionResponse>();
             allTransactions.AddRange((bankTransactionsQuery.ToList()).Select(x => new TransactionResponse(x)));
             allTransactions.AddRange((walletTransactionsQuery.ToList()).Select(x => new TransactionResponse(x)));
