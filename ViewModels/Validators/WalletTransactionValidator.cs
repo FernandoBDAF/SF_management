b3d2373e9c19e9c0f2684faa12a7649b2354af83
Lucas Costa <microsoft-e6656b3e.vehicular311@simplelogin.com>
2024-12-05 18:51:13 -0300

    improve wallet transaction validator.

--- ViewModels/Validators/WalletTransactionValidator.cs
+++ ViewModels/Validators/WalletTransactionValidator.cs
@@ -6,21 +6,10 @@ namespace SFManagement.ViewModels.Validators
     {
         public WalletTransactionValidator()
         {
-            RuleFor(x => x.Value).NotEmpty();
             RuleFor(x => x.Coins).NotEmpty();
-            RuleFor(x => x.ExchangeRate).NotEmpty();
-            RuleFor(x => x.Description).NotEmpty();
             RuleFor(x => x.Date).NotEmpty();
             RuleFor(x => x.WalletTransactionType).NotEmpty();
             RuleFor(x => x.WalletId).NotEmpty();
-
-            //RuleFor(x => x).Custom((walletTransaction, context) =>
-            //{
-            //    if (!walletTransaction.TagId.HasValue && !walletTransaction.ClientId.HasValue)
-            //    {
-            //        context.AddFailure($"Need send TagId or ClientId.");
-            //    }
-            //});
         }
     }
 }
