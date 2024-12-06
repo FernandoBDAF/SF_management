063aa9f173d513a43741a661abf381a92152ae1a
Lucas Costa <microsoft-e6656b3e.vehicular311@simplelogin.com>
2024-12-06 16:56:23 -0300

    add closing manager validators.

---
+++ ViewModels/Validators/ClosingManagerValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using SFManagement.Models;
+
+namespace SFManagement.ViewModels.Validators
+{
+    public class ClosingManagerValidator : AbstractValidator<ClosingManager>
+    {
+        public ClosingManagerValidator()
+        {
+            RuleFor(x => x.Start).LessThanOrEqualTo(x => x.End);
+        }
+    }
+}
