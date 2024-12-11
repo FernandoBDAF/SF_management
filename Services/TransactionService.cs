badc7eaa0eb335e17ffe9ca693343cc21c220f75
Lucas Costa <microsoft-e6656b3e.vehicular311@simplelogin.com>
2024-12-11 17:41:10 -0300

    add TODOS

--- Services/TransactionService.cs
+++ Services/TransactionService.cs
@@ -140,6 +140,10 @@ namespace SFManagement.Services
 
         public async Task<TableResponse<TransactionResponse>> GetInternalTransactions(Guid tagId, DateTime? startDate, DateTime? endDate, int quantity, int page)
         {
+            //TODO: BALANCE
+            //TODO: FILTER TAGID
+            //TODO: BALANCE TAG
+            
             var response = new TableResponse<TransactionResponse>()
             {
                 Page = page,
