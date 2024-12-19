0a20dca0f8258089cc1cf906155bb354a821228f
Lucas Costa <microsoft-e6656b3e.vehicular311@simplelogin.com>
2024-12-19 17:02:18 -0300

    fix update closing wallet.

--- Services/ClosingWalletService.cs
+++ Services/ClosingWalletService.cs
@@ -10,6 +10,24 @@ namespace SFManagement.Services
         {
         }
 
-        public async Task<List<ClosingWallet>> GetByClosingManagerId(Guid closingManagerId) => await _entity.Where(x => !x.DeletedAt.HasValue && x.ClosingManagerId == closingManagerId).Include(x=> x.Wallet).ToListAsync();
+        public async Task<List<ClosingWallet>> GetByClosingManagerId(Guid closingManagerId) => await _entity.Where(x => !x.DeletedAt.HasValue && x.ClosingManagerId == closingManagerId).Include(x => x.Wallet).ToListAsync();
+
+        public override async Task<ClosingWallet> Update(Guid id, ClosingWallet obj)
+        {
+            var existing = await context.ClosingWallets.FirstOrDefaultAsync(x => x.Id == id);
+
+            if(existing == null)
+            {
+                throw new AppException("Not found closing wallet.");
+            }
+
+            existing.ReturnRake = obj.ReturnRake;
+
+            context.ClosingWallets.Update(existing);
+
+            await context.SaveChangesAsync();
+
+            return existing;
+        }
     }
 }
