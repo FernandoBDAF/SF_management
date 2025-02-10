6b66c9cf09bd159a1c99e9c0b62f57fe19ba3a85
Fernando Barroso <fernandobarroso@suaquadra.com.br>
2025-02-10 13:27:38 -0800

    rerun

--- Authorization.cs
+++ Authorization.cs
@@ -10,6 +10,7 @@ namespace SFManagement
             ALL,
         }
 
+        
         public const string default_username = "user";
         public const string default_email = "user@secureapi.com";
         public const string default_password = "Pa$$w0rd.";
