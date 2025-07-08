9e865038b50169bcb62106d6744022135abc8698
Fernando Barroso <fernandobarroso@suaquadra.com.br>
2025-07-08 14:47:27 -0700

    redifine AssetType number's range

--- Enums/AssetType.cs
+++ Enums/AssetType.cs
@@ -2,11 +2,22 @@ namespace SFManagement.Enums;
 
 public enum AssetType
 {
+    // Fiat
     BrazilianReal = 1,
-    PokerStars = 2,
-    GgPoker = 3,
-    YaPoker = 4,
-    AmericasCardRoom = 5,
-    SupremaPoker = 6,
-    AstroPayICash = 7
+
+    // Poker
+    PokerStars = 101,
+    GgPoker = 102,
+    YaPoker = 103,
+    AmericasCardRoom = 104,
+    SupremaPoker = 105,
+    AstroPayICash = 106,
+
+    // Crypto
+    Bitcoin = 201,
+    Ethereum = 202,
+    Litecoin = 203,
+    Ripple = 204,
+    BitcoinCash = 205,
+    Stellar = 206,
 }
